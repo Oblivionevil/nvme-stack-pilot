acaa107460e8b56811c74e810dc58981a1434730
aufkrawall <13510490+aufkrawall@users.noreply.github.com>
2026-08-02 19:07:36 +0200

    Embed localization in single exe

--- src/NvmeDriverSwitch/Infrastructure/LocalizedStrings.cs
+++ src/NvmeDriverSwitch/Infrastructure/LocalizedStrings.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 using System.Resources;
 
@@ -5,17 +6,32 @@ namespace NvmeDriverSwitch.Infrastructure
 {
     /// <summary>
     /// Zugriff auf die UI-Ressourcen. Die Kultur wird von Windows (OS-Sprache) bestimmt;
-    /// deutsche Systeme erhalten die .de-Satellitenressourcen, alle anderen die neutrale
-    /// englische Ressource.
+    /// deutsche Systeme erhalten die deutsche Ressource, alle anderen die englische.
+    /// Beide Ressourcen liegen embedded in der EXE - es werden keine Satelliten-Assemblies
+    /// oder sonstige Zusatzdateien benoetigt.
     /// </summary>
     public static class LocalizedStrings
     {
-        private static readonly ResourceManager Manager =
+        private static readonly ResourceManager English =
             new ResourceManager("NvmeDriverSwitch.Properties.Resources", typeof(LocalizedStrings).Assembly);
+
+        private static readonly ResourceManager German =
+            new ResourceManager("NvmeDriverSwitch.Properties.ResourcesDe", typeof(LocalizedStrings).Assembly);
+
+        private static ResourceManager Current
+        {
+            get
+            {
+                return CultureInfo.CurrentUICulture.TwoLetterISOLanguageName
+                           .Equals("de", StringComparison.OrdinalIgnoreCase)
+                    ? German
+                    : English;
+            }
+        }
 
         public static string Get(string key)
         {
-            return Manager.GetString(key, CultureInfo.CurrentUICulture) ?? key;
+            return Current.GetString(key) ?? key;
         }
 
         public static string Format(string key, params object[] args)
