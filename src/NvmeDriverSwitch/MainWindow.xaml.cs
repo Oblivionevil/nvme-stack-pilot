3b53d8b06debdc078d918111250422e30dc557ec
aufkrawall <13510490+aufkrawall@users.noreply.github.com>
2026-08-02 18:51:13 +0200

    Auto-resize window height to fit content

--- src/NvmeDriverSwitch/MainWindow.xaml.cs
+++ src/NvmeDriverSwitch/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows;
 using NvmeDriverSwitch.ViewModels;
 
@@ -11,6 +12,13 @@ namespace NvmeDriverSwitch
         {
             InitializeComponent();
             DataContext = _vm;
+
+            // Die Hoehe folgt dem Inhalt, damit neue Bereiche nach einem Poll sofort
+            // sichtbar sind. Gedeckelt auf die Arbeitsflaeche minus Rand, damit das
+            // Fenster bei sehr langem Inhalt nicht ueber den Bildschirm hinauswaechst;
+            // erst dann uebernimmt der ScrollViewer.
+            MaxHeight = Math.Max(MinHeight, SystemParameters.WorkArea.Height - 24);
+
             Loaded += (s, e) => _vm.Start();
         }
     }
